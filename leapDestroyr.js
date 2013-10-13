eb43b33af9ea3aaaa03a8e4aab7ef0663bd61d07
Sameer <schauhan19@gmail.com>
2013-10-13 02:06:27 -0400

    Typo

--- leapDestroyr.js
+++ leapDestroyr.js
@@ -1,3 +1,84 @@
+//////////////////////////////////////////////////////////////
+//Leap Motion Stuff
+
+//Get the DOM object
+var canvas = document.getElementById('canvas');
+
+// Making sure we have the proper aspect ratio for our canvas
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+// Create the context we will use for drawing
+var c =  canvas.getContext('2d');
+
+
+// Save the canvas width and canvas height
+// as easily accesible variables
+var width = canvas.width;
+var height = canvas.height;
+
+// Creating a global Frame variable that we can access
+// throughout the program
+var frame;
+
+// Global keyTap and screenTap arrays
+var keyTaps = [];
+var KEYTAP_LIFETIME   =  .5;
+var KEYTAP_START_SIZE = 15;
+
+var screenTaps = [];
+var SCREENTAP_LIFETIME   = 1;
+var SCREENTAP_START_SIZE = 30;
+
+
+/*
+  
+  The leapToScene function takes a position in leap space 
+  and converts it to the space in the canvas.
+  
+  It does this by using the interaction box, in order to 
+  make sure that every part of the canvas is accesible 
+  in the interaction area of the leap
+
+  */
+
+  function leapToScene( leapPos ){
+
+  // Gets the interaction box of the current frame
+  var iBox = frame.interactionBox;
+
+  // Gets the left border and top border of the box
+  // In order to convert the position to the proper
+  // location for the canvas
+  var left = iBox.center[0] - iBox.size[0]/2;
+  var top = iBox.center[1] + iBox.size[1]/2;
+
+  // Takes our leap coordinates, and changes them so
+  // that the origin is in the top left corner 
+  var x = leapPos[0] - left;
+  var y = leapPos[1] - top;
+
+  // Divides the position by the size of the box
+  // so that x and y values will range from 0 to 1
+  // as they lay within the interaction box
+  x /= iBox.size[0];
+  y /= iBox.size[1];
+
+  // Uses the height and width of the canvas to scale
+  // the x and y coordinates in a way that they 
+  // take up the entire canvas
+  x *= width;
+  y *= height;
+
+  // Returns the values, making sure to negate the sign 
+  // of the y coordinate, because the y basis in canvas 
+  // points down instead of up
+  return [ x , -y ];
+
+}
+
+  
+
 (function() {
   var Bomb, Explosion, Particle, targetTime, vendor, w, _i, _len, _ref,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
@@ -368,4 +449,61 @@
 
   new Explosion();
 
+
+
+
+
 }).call(this);
+
+
+function onScreenTap( gesture ){
+
+  var pos  = leapToScene( gesture.position );
+  var time = frame.timestamp;
+  var eventObj = window.event;
+  eventObj.pageX = pos[0];
+  eventObj.pageY = pos[1];
+  eventObj.clientX = pos[0];
+  eventObj.clientY = pos[1];
+  Explosion.prototype.dropBomb(eventObj);
+
+  // screenTaps.push( [ pos[0] , pos[1] , time ] );
+
+}
+// Creates our Leap Controller
+var controller = new Leap.Controller({enableGestures:true});
+
+// Tells the controller what to do every time it sees a frame
+controller.on( 'frame' , function( data ){
+
+  // Assigning the data to the global frame object
+  frame = data;
+  
+  // Clearing the drawing from the previous frame
+  c.clearRect( 0 , 0 , width , height );
+
+  for( var i = 0; i < frame.gestures.length; i++ ){
+
+    var gesture = frame.gestures[i];
+
+
+    var type = gesture.type;
+    
+    switch( type ){
+      
+      case "screenTap":
+      onScreenTap( gesture );
+      break;
+      
+    }
+
+  }
+
+});
+
+// Get frames rolling by connecting the controller
+controller.connect();
+
+
+
+
